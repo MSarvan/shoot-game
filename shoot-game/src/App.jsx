8b2ca01b227aad02a9d7720bfeb2bf32f25634da
Saravanan M <sarvan.m20@gmail.com>
2022-05-28 10:27:18 +0530

    Basic setup

---
+++ shoot-game/src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className='box'>
+      </div>
+      <div className='circles'>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div className='inp'>
+        <input type="number" />
+        <br />
+        <button>Shoot</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
